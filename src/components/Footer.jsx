534e916099a4468d7b4631354a9af8a41a746a65
ankita67patil <ap289906@gmail.com>
2025-12-15 21:12:15 +0530

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,31 +5,29 @@ const Footer = () => {
   return (
     <footer className="py-5 sm:px-10 px-5">
       <div className="screen-max-width">
+        
+        {/* Info Section */}
         <div>
           <p className="font-semibold text-gray text-xs">
-            More ways to shop: {' '}
-            <span className="underline text-blue">
-            Find an Apple Store {' '}
-            </span>
-            or {' '}
-            <span className="underline text-blue">
-            other retailer
-            </span>{' '}
-            near you.
+            This is a frontend UI clone built for learning and educational purposes only.
           </p>
-          <p className="font-semibold text-gray text-xs">
-            Or call 000800-040-1966
+          <p className="font-semibold text-gray text-xs mt-1">
+            Not affiliated with or endorsed by Apple Inc.
           </p>
         </div>
 
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
 
-        <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
-          <div className="flex">
+        {/* Bottom Section */}
+        <div className="flex md:flex-row flex-col md:items-center justify-between gap-2">
+          <p className="font-semibold text-gray text-xs">
+            © 2024 UI Clone Project. All rights reserved.
+          </p>
+
+          <div className="flex flex-wrap">
             {footerLinks.map((link, i) => (
               <p key={link} className="font-semibold text-gray text-xs">
-                {link}{' '}
+                {link}
                 {i !== footerLinks.length - 1 && (
                   <span className="mx-2"> | </span>
                 )}
@@ -37,6 +35,7 @@ const Footer = () => {
             ))}
           </div>
         </div>
+
       </div>
     </footer>
   )
